b446b08271e3628b5a16277a5e419c04e4a7816f
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-01-16 22:55:42 +0100

    Add missing argument to bus_finish() prototype.

--- src/bus.h
+++ src/bus.h
@@ -14,7 +14,7 @@ typedef struct bus_tag bus_t;
 uint8_t bus_read_1(bus_t *, uint16_t);
 void bus_write_1(bus_t *, uint16_t, uint8_t);
 bus_t bus_init();
-void bus_finish();
+void bus_finish(bus_t *);
 
 #endif /* _BUS_H_ */
 
