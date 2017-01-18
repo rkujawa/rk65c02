e7380477a932b31366692df2fb3f3cf0615c1b98
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-01-19 00:20:53 +0100

    Remove debug printfs.

--- src/emulation.c
+++ src/emulation.c
@@ -6,15 +6,10 @@ void
 emul_lda(rk65c02emu_t *e, instruction_t *i)
 {
 	instrdef_t id;
-
 	id = instruction_decode(i->opcode);
 
-	printf("A: %x", e->regs.A);
-
 	e->regs.A = i->op1;
-
-	printf("A: %x", e->regs.A);
-
+ 
 }
 
 void
