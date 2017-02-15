3286891ea8d17d271c4c4ab6c1cb499b1cc74eb2
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-02-15 19:55:48 +0100

    Add missing prototype of debug_breakpoint_remove to include file.

--- src/debug.h
+++ src/debug.h
@@ -8,5 +8,7 @@
 
 bool debug_PC_is_breakpoint(rk65c02emu_t *);
 bool debug_breakpoint_add(rk65c02emu_t *, uint16_t);
+bool debug_breakpoint_remove(rk65c02emu_t *, uint16_t);
+
 #endif /* _DEBUG_H_ */
 
