b3e49fdb249b1d02d1479752d90b78669a2b74a0
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2019-09-23 15:28:05 +0200

    Add preambles with license information.

--- src/device_serial.c
+++ src/device_serial.c
@@ -1,3 +1,21 @@
+/*
+ *      SPDX-License-Identifier: GPL-3.0-only
+ *
+ *      rk65c02
+ *      Copyright (C) 2017-2019  Radoslaw Kujawa
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, version 3 of the License.
+ * 
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
