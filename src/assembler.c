6ccb94ac461fc566f59792b5b5bc39a32d21ec9d
Radoslaw Kujawa <radoslaw.kujawa@c0ff33.net>
2021-01-25 01:09:01 +0100

    Update copyright year.

--- src/assembler.c
+++ src/assembler.c
@@ -2,7 +2,7 @@
  *	SPDX-License-Identifier: GPL-3.0-only
  *
  *	rk65c02
- *	Copyright (C) 2017-2019  Radoslaw Kujawa
+ *	Copyright (C) 2017-2021  Radoslaw Kujawa
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
